3d9e93147f141694b35559ebb1fd5bfba57dd243
Birdperson Mathis <birdpers@si-a3.nsk.21-school.ru>
2024-01-20 15:45:08 +0700

    containers added

---
+++ src/s21_containers.h
@@ -0,0 +1,10 @@
+#ifndef SRC_S21_CONTAINERS_H
+#define SRC_S21_CONTAINERS_H
+
+#include "implementations/s21_map.h"
+#include "implementations/s21_queue.h"
+#include "implementations/s21_set.h"
+#include "implementations/s21_stack.h"
+#include "implementations/s21_vector.h"
+
+#endif  // SRC_S21_CONTAINERS_H_
---
+++ src/s21_containersplus.h
@@ -0,0 +1,7 @@
+#ifndef SRC_S21_CONTAINERSPLUS_H
+#define SRC_S21_CONTAINERSPLUS_H
+
+#include "implementations/s21_array.h"
+#include "implementations/s21_multiset.h"
+
+#endif  // SRC_S21_CONTAINERSPLUS_H_
